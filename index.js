57655662c17dfad5ac8465d2633b9a8132bad40f
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-01-26 11:45:48 +0600

    news-categores api

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const data = require("./data/categories.json");
+
 //middleware
 app.use(cors());
+
+//news categories api
+app.get("/news-categories", (req, res) => {
+    res.send(data);
+})
 
 app.get("/", (req, res) => {
     res.send("News Portal Server is running");
